ff87a3317b4cff267289783e569b1f808bfa5366
maryem.ABDENNABI <maryem.abdennabi@esprit.tn>
2022-11-12 14:27:09 +0100

    8

--- src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
+++ src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
@@ -31,8 +31,7 @@ public class SpringFoxSwaggerConfig {
 				.securityContexts(Collections.singletonList(securityContext()))
 				.securitySchemes(Arrays.asList(apiKey()))
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("tn.esprit.rh.achat.controllers
-"))
+				.apis(RequestHandlerSelectors.basePackage("tn.esprit.rh.achat.controllers")
 				.paths(PathSelectors.any())
 				.build();
 	}
