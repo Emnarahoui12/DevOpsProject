27a423effd94f82061c9aa52ff300428cac0e5c9
w3med <med@null.computer>
2022-11-15 00:34:28 +0100

    init branch + cleaning

---
+++ src/test/java/tn/esprit/rh/achat/services/Secteur/SecteurActiviteServiceTest.java
@@ -0,0 +1,17 @@
+package tn.esprit.rh.achat.services.secteur;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Secteur;
+import tn.esprit.rh.achat.services.ISecteurActiviteService;
+
+import java.util.List;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class SecteurActiviteServiceTest {s
+
+
+
+}
