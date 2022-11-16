ebd99e16c8b556f6090435be7884c95ed6b83171
maryem.ABDENNABI <maryem.abdennabi@esprit.tn>
2022-11-16 14:43:35 +0100

    tests

--- src/test/java/tn/esprit/rh/achat/facture/FactureTest.java
+++ src/test/java/tn/esprit/rh/achat/facture/FactureTest.java
@@ -1,6 +1,5 @@
 package tn.esprit.rh.achat.facture;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -10,30 +9,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 
 import lombok.extern.slf4j.Slf4j;
 import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.repositories.FactureRepository;
 import tn.esprit.rh.achat.services.FactureServiceImpl;
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
 
 @TestMethodOrder(OrderAnnotation.class)
 //@ExtendWith(MockitoExtension.class)
 @RunWith(MockitoJUnitRunner.class)
 @Slf4j
- class FactureTest {
+public class FactureTest {
 	
 	FactureRepository factureRepository=Mockito.mock(FactureRepository.class);
 
@@ -42,7 +37,7 @@ import tn.esprit.rh.achat.services.FactureServiceImpl;
 	
 	
 	@Test
-	 void TestRetrieveFacture() {
+	public void TestRetrieveFacture() {
 
 	Facture facture = new Facture(null, 12, 300, null, null, true, null, null, null);
 	facture.setIdFacture(1L);
@@ -58,7 +53,7 @@ import tn.esprit.rh.achat.services.FactureServiceImpl;
 	
 	
 	@Test
-	 void TestRetrieveAllFactures()
+	public void TestRetrieveAllFactures()
 	{
 	List<Facture> factures = new ArrayList<Facture>() {
 
@@ -67,34 +62,49 @@ import tn.esprit.rh.achat.services.FactureServiceImpl;
 	add(new Facture(null, 3000, 60000, null, null, true, null, null, null));
 	add(new Facture(null, 4000, 70000, null, null, true, null, null, null));
 	}};
+
+	
+	
+
 	when(factureService.retrieveAllFactures()).thenReturn(factures);
 	//test
 	List<Facture> facturesList = factureService.retrieveAllFactures();
 	assertEquals(3,facturesList.size());
 	log.info("NB FACTURES : "+String.valueOf(factures.size()));
+	
 	log.info(" TestRetrieveAllFactures - TEST SUCCESS");
+
+
 	}
 	
 	
 	@Test
-	 void TestAddFacture()
+	public void TestAddFacture()
 	{
+
 		Facture facture = new Facture(null, 120, 3000, null, null, false, null, null, null);
 		facture.setIdFacture(2L);
+
 		factureService.addFacture(facture);
 	verify(factureRepository, times(1)).save(facture);
+	
 	log.info(" TestAddFacture - TEST SUCCESS");
 	}
 	
 	
 	@Test
-     void TestDeleteFacture(){
+	public void TestDeleteFacture(){
 
 	Facture facture = new Facture(null, 4000, 70000, null, null, true, null, null, null);
 	facture.setIdFacture(7L);
+
+
+
 	Mockito.lenient().when(factureRepository.findById(facture.getIdFacture())).thenReturn(Optional.of(facture));
+
 	factureService.deleteFacture(7L);
 	verify(factureRepository).deleteById(facture.getIdFacture());
+
 	log.info("  TestDeleteFacture - TEST SUCCESS ");
 	}
 	
